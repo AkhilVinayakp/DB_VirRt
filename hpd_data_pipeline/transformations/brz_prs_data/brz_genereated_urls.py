737fe60ba8e180ab04ca9c7373b48239f9a2e648
AkhilVinayakp <AkhilVinayakp>
2025-08-16 17:42:58 +0000

    DLT fix

--- hpd_data_pipeline/transformations/brz_prs_data/brz_genereated_urls.py
+++ hpd_data_pipeline/transformations/brz_prs_data/brz_genereated_urls.py
@@ -1,46 +1,23 @@
 import dlt
 from pyspark.sql import functions as F
 
-@dlt.table(
-    comment="Streaming table that generates URLs based on zip and state data",
-    temporary=False
-)
+@dlt.table
 def brz_genereated_urls():
-    # Read configuration from DLT pipeline settings
     api_url_template = spark.conf.get("api_url")
     referal_url_template = spark.conf.get("referal_url")
-    zip_load_count = int(spark.conf.get("zip_load_count"))
 
-    # Read streaming source
+    # Stream CSV files from Volume (not checkpoint folder!)
     source_df = (
-        spark.read
-        .table("uszips")
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("header", "true")
+        .load("/Volumes/mycatalog/hp_prd_data/checkpoint_v/zips")
     )
 
-    # Filter, randomize, and limit
-    sub_df = (
+    return (
         source_df
-        .filter(F.col("population").isNotNull())
-        .withColumn("state_name", F.regexp_replace(F.col("state_name"), " ", "-"))
-        .orderBy(F.rand())
-        .limit(zip_load_count)
+        .withColumn("api_url", F.format_string(api_url_template, F.col("zip").cast("string")))
+        .withColumn("refereral_url", F.format_string(referal_url_template, F.col("zip").cast("string"), F.col("state_id").cast("string"), F.col("state_name")))
+        .withColumn("processed_datehour", F.date_format(F.current_timestamp(), "yyyy-MM-dd-HH"))
+        .withColumn("batch_id", F.current_timestamp())
     )
-
-    # Build URLs
-    final_df = (
-        sub_df
-        .withColumn(
-            "api_url",
-            F.format_string(api_url_template, F.col("zip"))
-        )
-        .withColumn(
-            "refereral_url",
-            F.format_string(referal_url_template, F.col("zip"), F.col("state_id"), F.col("state_name"))
-        )
-        .withColumn(
-            "processed_datehour",
-            F.date_format(F.current_timestamp(), "yyyy-MM-dd-HH")
-        )
-    )
-
-    return final_df
